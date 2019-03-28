72f8a96940f2ba8c1d02d70be4bee43fca8cf339
ReinholdChen <2630966248@qq.com>
2019-03-28 15:30:14 +0800

    再次优化结构

--- footer.php
+++ footer.php
@@ -10,12 +10,15 @@
     <div data-value="设置备案信息" style="width:300px;margin:0 auto; padding:20px 0;">
         <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011302001914"
            style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-            <img alt="图片未找到" src="/image/beian.png" style="float:left;"/>
+            <img alt="图片未找到" src="/web/image/beian.png" style="float:left;"/>
             <p style="float:left;height:20px;line-height:20px;margin: 0 0 0 5px; color:#939393;">粤公网安备
                 44011302001914号</p><br>
-            <img alt="图片未找到" src="/image/my_icon.png" style="float:left;"/>
-            <p style="float:left;height:20px;line-height:20px;margin: 0 0 0 5px; color:#939393;">
-    联系方式：2630966248@qq.com</p>
+        </a>
+        <a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=WzgzPjV2Ii52ODMyGyoqdTg0Ng"
+           style="text-decoration:none;">
+            <img src="http://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_01.png"
+                 style="float:left;"/>
+            <p style="float:left;height:20px;line-height:20px;margin: 0 0 0 5px; color:#939393;">←反馈渠道</p>
         </a>
     </div>
 </footer>
---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="/image/favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="/image/favicon.ico" type="image/x-icon"/>
+    <title>项目主界面</title>
+</head>
+<body>
+<h2>从以下列表选取访问:</h2>
+<br>
+<ul id="main"></ul>
+<li>
+    <a href="/index.php">wordpress更新说明</a>
+</li>
+<li>
+    <a href="/web/book/index.php">图书查询系统</a>
+</li>
+<p>by 陈南华</p>
+
+<?php
+include 'footer.php';
+?>
+</body>
+</html>
