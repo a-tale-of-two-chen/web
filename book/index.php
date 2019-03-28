0eee29be9e27f322774cdd6f0b06508bcf507c33
ReinholdChen <2630966248@qq.com>
2019-03-28 22:06:26 +0800

    接近完成，只是没做分页展示

--- book/index.php
+++ book/index.php
@@ -15,14 +15,129 @@
             text-align: center;
             text-decoration: none;
         }
+
+        tr {
+            width: 14px;
+            font-size: 18px;
+            word-wrap: break-word;
+        }
     </style>
     <title>图书查询系统的网页实现</title>
+
 </head>
 <body>
-<p>仅包含查询逻辑</p>
-<input style="user-select: text">
+<h2>欢迎使用：</h2>
+<p>
+    <small>注：仅包含查询逻辑</small>
+</p>
+<table border="10">
+    <tr>
+        <td>编号</td>
+        <td>类型</td>
+        <td>书名</td>
+        <td>作者</td>
+        <td>评分</td>
+        <td width="700px">简介</td>
+        <td>网址</td>
+    </tr>
+    <?php
+    $num1 = rand(2, 16553);
+    $num2 = rand(2, 16553);
+    $num3 = rand(2, 16553);
+
+    $db = new mysqli('127.0.0.1', 'root', '51130012cyc', 'book');
+    if (mysqli_connect_error()) {
+        echo "<h3> Mysql无连接，请稍后尝试。</h3><br>" .
+            "感谢你的使用。";
+        exit();
+    }
+    $query = "select * from books where ID = ? or ID = ? or ID = ?";
+    mysqli_query($db, "set names'utf8'");
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('ddd', $num1, $num2, $num3);
+    $stmt->execute();
+    $stmt->bind_result($ID, $Kind, $Name, $Author, $Score, $Syno, $Site);
+    while ($stmt->fetch()) {
+        echo "<tr>
+		  <td>$ID</td>
+		  <td>$Kind</td>
+		  <td>$Name</td>
+		  <td>$Author</td>
+		  <td>$Score</td>
+		  <td>$Syno</td>
+		  <td><a href='$Site'>$Site</a></td>
+		  </tr>";
+    }
+    ?>
+</table>
+<h3>从以下分类中确定要浏览的对象后开始搜索</h3>
+<table  border="0">
+    <tr id="Kind">
+        <td>小说</td>
+        <td>随笔</td>
+        <td>散文</td>
+        <td>诗歌</td>
+        <td>童话</td>
+        <td>名著</td>
+        <td>港台</td>
+        <td>漫画</td>
+        <td>推理</td>
+        <td>绘本</td>
+        <td>青春</td>
+        <td>科幻</td>
+        <td>言情</td>
+        <td>奇幻</td>
+        <td>武侠</td>
+        <td>历史</td>
+        <td>哲学</td>
+        <td>传记</td>
+        <td>设计</td>
+        <td>建筑</td>
+        <td>电影</td>
+        <td>音乐</td>
+        <td>旅行</td>
+        <td>励志</td>
+        <td>教育</td>
+        <td>职场</td>
+        <td>美食</td>
+        <td>灵修</td>
+        <td>健康</td>
+        <td>家居</td>
+        <td>管理</td>
+        <td>商业</td>
+        <td>金融</td>
+        <td>营销</td>
+        <td>理财</td>
+        <td>股票</td>
+        <td>科普</td>
+        <td>编程</td>
+        <td>算法</td>
+        <td>通信</td>
+        <td>回忆录</td>
+        <td>经济学</td>
+        <td>企业史</td>
+        <td>互联网</td>
+        <td>交互设计</td>
+        <td>神经网络</td>
+        <td>日本文学</td>
+    </tr>
+</table>
+<h3>开始搜索：(选择类型后输入对应类型可以浏览该类型的书籍，模糊匹配需要用%...%<br>包围被搜索字符串不要问我为什么不用JS做，瓜皮JS。</h3>
+<p>选择执行条件</p>
+<form action="search.php" method="get">
+    <select name="范围">
+        <option value="全部" selected="selected">全部</option>
+        <option value="书名">书名</option>
+        <option value="作者">作者</option>
+        <option value="简介">简介</option>
+        <option value="类型">类型</option>
+    </select>
+    <input id="search" name="search" type="text" maxlength="20" value="">
+    <input type="submit" src="../image/my_icon.png" value="开始搜索">
+</form>
 <?php
 include '../footer.php';
 ?>
 </body>
+
 </html>
--- book/search.php
+++ book/search.php
@@ -6,58 +6,85 @@
  * Time: 23:19
  * 第一阶段的完整代码
  */
-if(isset($_GET['search'])){
-	$search = trim($_GET['search']);
-}else{
-	echo "<p>请输入正确参数</p>";
-}
 
-
-$db = new mysqli('127.0.0.1','root','51130012cyc','book');
-if(mysqli_connect_error()){
-	echo "<h3> Mysql无连接，请稍后尝试。</h3><br>".
-		"感谢你的使用。";
-	exit();
+//连接数据库
+$db = new mysqli('127.0.0.1', 'root', '51130012cyc', 'book');
+if (mysqli_connect_error()) {
+    echo "<h3> Mysql无连接，请稍后尝试。</h3><br>" .
+        "感谢你的使用。";
+    exit();
 }
-$query = "select * from books where Name = ? or ID = ? or Author = ? or Score = ? or Syno = ? or Site = ? limit 1";
-mysqli_query($db,"set names'utf8'");
-$stmt = $db->prepare($query);
-$stmt->bind_param('ssssss',$search,$search,$search,$search,$search,$search);
+//判断输入
+if (isset($_GET['search'])) {
+    $search = trim($_GET['search']);
+    $范围     = trim($_GET['范围']);
+} else {
+    echo "<p>请输入正确参数</p>";
+}
+//分别指定不同流程
+if ($范围 == "全部") {
+    $query = "select * from books where Name like ? or ID like ? or Kind like ? or Author like ? or Score like ? or Syno like ? or Site like ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('sssssss', $search, $search, $search, $search, $search, $search, $search);
+
+} else if ($范围 == "书名") {
+    $query = "select * from books where Name like ?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $search);
+} else if ($范围 == "作者") {
+    $query = "select * from books where Author like?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $search);
+} else if ($范围 == "简介") {
+    $query = "select * from books where Syno like ?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $search);
+} else if ($范围 == "类型") {
+    $query = "select * from books where  Kind like ?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $search);
+}
+mysqli_query($db, "set names'utf8'");
 $stmt->execute();
-$stmt->bind_result($ID,$Name,$Author,$Score,$Syno,$Site);
+$stmt->bind_result($ID, $Kind, $Name, $Author, $Score, $Syno, $Site);
 ?>
 <!DOCTYPE html>
 <html lang="UTF-8">
 <head>
-	<title>结果集</title>
+    <title>结果集</title>
 </head>
 <body>
 <h1>查询结果：</h1>
 <br>
-<table border="1">
-	<tr>
-		<td>编号</td>
-		<td>书名</td>
-		<td>作者</td>
-		<td>评分</td>
-		<td width="700px">简介</td>
-		<td>网址</td>
-	</tr>
-<?php
-//echo "一共得到".mysqli_stmt_affected_rows($db)."行数据<br>";
-while ($stmt->fetch()){
-	echo "<tr>
+<table border="10">
+    <tr>
+        <td>编号</td>
+        <td>类型</td>
+        <td>书名</td>
+        <td>作者</td>
+        <td>评分</td>
+        <td width="700px">简介</td>
+        <td>网址</td>
+    </tr>
+    <?php
+    //echo "一共得到".mysqli_stmt_affected_rows($db)."行数据<br>";
+    $total_num = 0;
+    while ($stmt->fetch()) {
+        $total_num++;
+        echo "<tr>
 		  <td>$ID</td>
+		  <td>$Kind</td>
 		  <td>$Name</td>
 		  <td>$Author</td>
 		  <td>$Score</td>
 		  <td>$Syno</td>
 		  <td><a href='$Site'>$Site</a></td>
 		  </tr>";
-}
-$stmt->free_result();
-$db->close();
-?>
+    }
+    echo "<p>一共得到" . $total_num . "行数据</p><br>";
+    $stmt->free_result();
+    $db->close();
+    ?>
 </table>
 <?php
 include '../footer.php';
