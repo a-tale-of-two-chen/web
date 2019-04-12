cc9593972cb29dd8828db830cdd5fe6e7c1d2268
ReinholdChen <2630966248@qq.com>
2019-04-13 00:02:40 +0800

    支持持久访问

--- user/login.php
+++ user/login.php
@@ -1,24 +1,52 @@
 <?php
-    if(isset($_POST['name'])){
-        @$name=$_POST["name"];
-        @$password=$_POST["password"];
-
-        include '../mysql.php';
-        $db->select_db('user');
-        $query="select password from user.users where name=?";
-        $stmt=$db->prepare($query);
-        $stmt->bind_param('s',$name);
-        $stmt->execute();
-        $stmt->store_result();                      //缓存结果
-        $stmt->bind_result($password_db);    //结果绑定变量
-        $stmt->fetch();                              //取结果
-        if(password_verify($password,$password_db)){
-            $_SESSION["name"] = $name;
-            echo "<h1>登录成功!</h1>";
-            echo "<meta charset='UTF-8' http-equiv='refresh' content=\"1;url=/web/index.php\">";
-            exit();
-        }
+if(isset($_COOKIE['name'])){
+    $name=$_COOKIE['name'];
+    include '../mysql.php';
+    $db->select_db('user');
+    $query = "select password from user.users where name=?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    if ($stmt->affected_rows == 0) {             //判断有没有该用户
+        echo "<h1>没有该用户!</h1>";
+        echo "<meta charset='UTF-8' http-equiv=\"refresh\" content=\"1;url=login.php\">";
+        exit();
+    }else{
+        $_SESSION["name"] = $name;
+        echo "<h1>登录成功!</h1>";
+        echo "<meta charset='UTF-8' http-equiv='refresh' content=\"1;url=/web/index.php\">";
+        exit();
     }
+}
+if (isset($_POST['name'])&&!isset($_COOKIE['name'])) {
+    @$name = $_POST["name"];
+    @$password = $_POST["password"];
+
+    include '../mysql.php';
+    $db->select_db('user');
+    $query = "select password from user.users where name=?";
+    $stmt  = $db->prepare($query);
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    if ($stmt->affected_rows == 0) {             //判断有没有该用户
+        echo "<h1>没有该用户!</h1>";
+        echo "<meta charset='UTF-8' http-equiv=\"refresh\" content=\"1;url=login.php\">";
+        exit();
+    }
+    $stmt->store_result();                       //缓存结果
+    $stmt->bind_result($password_db);     //结果绑定变量
+    $stmt->fetch();                             //取结果
+    if (password_verify($password, $password_db)) {
+        $_SESSION["name"] = $name;
+        echo "<h1>登录成功!</h1>";
+        echo "<meta charset='UTF-8' http-equiv='refresh' content=\"1;url=/web/index.php\">";
+        exit();
+    } else {
+        echo "<h2>密码错误！</h2>";
+        echo "<a href='login.php'>返回</a>";
+        exit();
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -63,8 +91,8 @@
                     <input type="hidden" name="flag" value="login" required>
                     <input type="submit" value="登陆" name="login">
                     </br>
-                    <p class="bstyle"><input type="checkbox" name="rempas" />  记住密码</p>
-                    <p class="cstyle"><a href="forget.php">忘记密码</a> </p>
+                    <p class="bstyle"><input type="checkbox" name="rempas"/>  记住密码</p>
+                    <p class="cstyle"><a href="forget.php">忘记密码</a></p>
                     </br></br>
                     <p class="cstyle">没有账号？<a href="register.php?register=register">立即注册</a></p>
                 </form>
@@ -76,7 +104,9 @@
     <!-- 尾部 -->
     <div class="footer">
         <ul>
-            <li><pre>                                                             </pre></li>
+            <li>
+                <pre>                                                             </pre>
+            </li>
             <li>powered by cscnh.cn|</li>
             <li>2018 © Charon</li>
         </ul>
